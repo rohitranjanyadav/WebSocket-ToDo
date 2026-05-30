6203c6eb2da8f05e6cf2521b4a05ae9f76fbbaa4
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-05-30 23:18:29 +0545

    feat: implement deleteTodo functionality in todoController

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -10,27 +10,56 @@ class Todo {
       console.log("New Client Connected!");
 
       socket.on("addTodo", (data) => this.handleAddTodo(socket, data));
+      socket.on("deleteTodo", (data) => this.handleDeleteTodo(socket, data));
     });
   }
 
   private async handleAddTodo(socket: Socket, data: ITodo) {
     const { task, deadLine, status } = data;
 
-    try{await todoModel.create({
-      task,
-      deadLine,
-      status,
-    });
-    const todos = await todoModel.find()
+    try {
+      await todoModel.create({
+        task,
+        deadLine,
+        status,
+      });
+      const todos = await todoModel.find();
 
-    socket.emit("todos_updated",{
-      status:"success",
-      data:todos
-    })}catch(error){
-      socket.emit("todo_response",{
-        status:"error",
-        error
-      })
+      socket.emit("todos_updated", {
+        status: "success",
+        data: todos,
+      });
+    } catch (error) {
+      socket.emit("todo_response", {
+        status: "error",
+        error,
+      });
+    }
+  }
+
+  private async handleDeleteTodo(socket: Socket, data: { id: string }) {
+    try {
+      const { id } = data;
+
+      const deletedTodo = await todoModel.findByIdAndDelete(id);
+      if (!deletedTodo) {
+        socket.emit("todo_response", {
+          status: "error",
+          message: "Todo Not Found!",
+        });
+        return;
+      }
+      const todos = await todoModel.find();
+
+      socket.emit("todos_updated", {
+        status: "success",
+        data: todos,
+      });
+    } catch (error) {
+      socket.emit("todo_response", {
+        status: error,
+        error,
+      });
     }
   }
 }
